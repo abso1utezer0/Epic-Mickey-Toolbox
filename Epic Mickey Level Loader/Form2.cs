4e2ff1a7875fc28dfb7e114d5b0154a91c7aebfa
Kjøb <57041363+KjubDusJub@users.noreply.github.com>
2022-03-31 14:18:23 +0200

    Improved output messages in main window

--- Epic Mickey Level Loader/Form2.cs
+++ Epic Mickey Level Loader/Form2.cs
@@ -34,22 +34,25 @@ namespace Epic_Mickey_Level_Loader
         }
         void Init()
         {
+           
             if (Settings1.Default.DolphinPath == "")
             {
                 label1.Text = "Please define your dolphin.exe path in Settings";
             }
             else
             {
-                if (Settings1.Default.EMPath == "")
-                {
-                    label1.Text = "Epic Mickey not found. Either download the game by pressing the download button or select your main.dol in the settings";
-                }
-                else
+                if(Settings1.Default.EMPath != "")
                 {
                     label1.Text = "Ready to play";
                     isReady = true;
                 }
             }
+            if (Settings1.Default.EMPath == "")
+            {
+                label1.Text = "Epic Mickey not found. Either download the game by pressing the download button or select your main.dol in the settings";
+            }
+        
+
         }
         void OnChange(object sender, EventArgs e)
         {
